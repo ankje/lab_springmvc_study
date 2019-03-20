3a5610c72a7dff510864b3e13baea09c7465ce0f
吴健勇 <ankje@qq.com>
2019-03-20 23:23:42 +0800

    原生jdbc连接mysql

---
+++ src/com/ankje/mybatis/study/NativeJDBC.java
@@ -0,0 +1,136 @@
+package com.ankje.mybatis.study;
+
+import org.json.simple.JSONValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewRendererServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("native-jdbc")
+public class NativeJDBC {
+
+    @RequestMapping(value = {
+        "","/"
+    })
+    public ModelAndView index(HttpServletResponse pResp){
+        //数据库连接
+        Connection connection = null;
+        //预编译的Statement，使用预编译的Statement提高数据库性能
+        PreparedStatement preparedStatement = null;
+        //结果集
+        ResultSet resultSet = null;
+
+        String resultSetJson = "";
+
+        try {
+            //加载数据库驱动
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //通过驱动管理类获取数据库链接
+            connection =  DriverManager.getConnection("jdbc:mysql://192.168.112.135:3306/mybatis001?characterEncoding=utf-8", "root", "root");
+            //定义sql语句 ?表示占位符
+            String sql = "select * from user where username = ?";
+            //获取预处理statement
+            preparedStatement = connection.prepareStatement(sql);
+            //设置参数，第一个参数为sql语句中参数的序号（从1开始），第二个参数为设置的参数值
+            preparedStatement.setString(1, "wangwu");
+            //向数据库发出sql执行查询，查询出结果集
+            resultSet =  preparedStatement.executeQuery();
+
+            resultSetJson = JSONValue.toJSONString(convertList(resultSet));
+
+            //遍历查询结果集
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("id")+"  "+resultSet.getString("username"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            //释放资源
+            if(resultSet!=null){
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            if(preparedStatement!=null){
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            if(connection!=null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        HttpServletRequest req = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpServletResponse resp = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getResponse();
+
+        //返回json
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("application/json;charset=utf-8");
+        try {
+            resp.getWriter().write(resultSetJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+        //使用绝对路径的视图
+//        try {
+//            req.getRequestDispatcher("/WEB-INF/jsp/native-jdbc/index.jsp").forward(req,resp);
+//        } catch (ServletException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+
+        //简写视图路径
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject("json",resultSetJson);
+//        modelAndView.setViewName("native-jdbc/index");
+//        return modelAndView;
+    }
+
+    //将ResultSet转换为List
+    private static List convertList(ResultSet rs) throws SQLException{
+        List list = new ArrayList();
+        ResultSetMetaData md = rs.getMetaData();//获取键名
+        int columnCount = md.getColumnCount();//获取行的数量
+        while (rs.next()) {
+            Map rowData = new HashMap();//声明Map
+            for (int i = 1; i <= columnCount; i++) {
+                rowData.put(md.getColumnName(i), rs.getObject(i));//获取键名及值
+            }
+            list.add(rowData);
+        }
+        return list;
+    }
+}
---
+++ web/WEB-INF/jsp/native-jdbc/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Ankje
+  Date: 2019/3/20 0020
+  Time: 下午 5:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+native-jdbc
+${json}
+</body>
+</html>
