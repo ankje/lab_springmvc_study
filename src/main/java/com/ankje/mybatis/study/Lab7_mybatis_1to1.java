d9df512984f8928af8a7d6b93ced9d316d4eb4f1
吴健勇 <ankje@qq.com>
2019-03-29 19:36:10 +0800

    mybatis 1to1

--- src/main/java/com/ankje/mybatis/study/Lab7_mybatis_1to1.java
+++ src/main/java/com/ankje/mybatis/study/Lab7_mybatis_1to1.java
@@ -1,21 +1,22 @@
-/*
 package com.ankje.mybatis.study;
 
 import com.ankje.mybatis.study.model.UserCustom;
 import com.ankje.mybatis.study.model.UserQueryVo;
-import com.ankje.mybatis.study.model.dao_mapper.lab6.UserMapper;
+import com.ankje.mybatis.study.model.lab7.mapper.OrdersMapper;
+import com.ankje.mybatis.study.model.lab7.mapper.UserMapper;
+import com.ankje.mybatis.study.model.lab7.po.Orders;
+import com.ankje.mybatis.study.model.lab7.po.OrdersExample;
+import com.ankje.mybatis.study.model.lab7.po.UserExample;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -25,20 +26,26 @@ public class Lab7_mybatis_1to1 {
     private SqlSessionFactory sqlSessionFactory;
 
     public Lab7_mybatis_1to1() throws IOException {
-        InputStream inputStream = Resources.getResourceAsStream("lab7/mybatisConfig.xml");
+        InputStream inputStream = Resources.getResourceAsStream("lab7_mybatisConfig.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         this.sqlSessionFactory = sqlSessionFactory;
     }
 
-    //*******查
+    //*******查 联查order和user表
     @ResponseBody
-    @RequestMapping(value = {"user_list_ids"},method = {RequestMethod.POST})
-    public List<UserCustom> findUserList(@RequestBody List<Integer> ids) throws Exception {
+    @RequestMapping(value = {"orderListWithUser/{lessId}/{userId}"},method = {RequestMethod.GET})
+    public List<Orders> orderListWithUser(@PathVariable("lessId") int lessId,@PathVariable("userId") int userId) throws Exception {
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        List<UserCustom> users= userMapper.findUserListWhereForeach(new UserQueryVo().setIds(ids));
+        OrdersMapper Mapper = sqlSession.getMapper(OrdersMapper.class);
+        OrdersExample ordersExample = new OrdersExample();
+        OrdersExample.Criteria orderCriteria = ordersExample.createCriteria();
+        orderCriteria.andIdLessThan(lessId);
+        UserExample userExample = new UserExample();
+        UserExample.Criteria userCriteria = userExample.createCriteria();
+        if(userId>0)
+            userCriteria.andIdEqualTo(userId);
+        List<Orders> orders= Mapper.selectByWhereWithUser(ordersExample,userExample);
         sqlSession.close();
-        return users;
+        return orders;
     }
 }
-*/
